c2a965a630e44a3760721bc4b9b5406dddee0b9a
Filip Krunic <fkrunic9@gmail.com>
2023-09-01 10:55:22 -0700

    project setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>  
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      bodyClass: ''
+    };
+  },
+  mounted() {
+    document.body.className = this.bodyClass;
+  },
+  beforeDestroy() {
+    // Reset the class when the component is destroyed
+    document.body.className = '';
+  }
+};
+</script>
